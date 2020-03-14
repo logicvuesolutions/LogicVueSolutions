2756cdc4259fe951f9e51da4f0cf2c3c87002c84
Taylor Desens <taylor.desens@gmail.com>
2020-03-14 18:14:25 -0400

    Style improvements
    Add grey backgrounds to new pages; condense pages into single template; add images/

--- page.php
+++ page.php
@@ -1,36 +1,51 @@
-<?php 
-/* Template Name: Page */ 
-?>
 
 <?php get_header(); ?>
 
 
-<div class="content" >
-	<div class="container-fluid" >
-		<div class="intro-block" >
-			<div class="container" >
-				<div class="row" >
-					<div class="col-sm" >
-						<?php the_field('intro'); ?>
-					</div><!-- .col-sm -->
-				</div><!-- .row -->
-			</div><!-- .container -->
-		</div><!-- .intro-block -->
-	</div><!-- .container-fluid -->
+
+<?php get_header(); ?>
 
 
-	<div class="container" >
+	<div id="solutions-menu"style="top:90px;">
+		<?php wp_nav_menu( array( 'theme_location' => 'solutions' ) ); ?>
+	</div><!-- #solutions-menu -->
+
+
+<div class="intro" id="<?php the_field('id'); ?>-intro" >
+	<div class="container centered" >
 		<div class="row" >
-			<div class="col-sm" >
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-					<?php the_content(); ?>
-
-				<?php endwhile; endif; ?>
-			</div><!-- .col-sm -->
+			<div class="col-lg-5 intro-text" >
+					<?php the_field('intro'); ?>
+			</div><!-- .col -->
+			<div class="col" >
+				<div class="image-wrapper" style="background-image:url(<?php the_field('intro_image'); ?>);" ></div>
+			</div><!-- .col -->
 		</div><!-- .row -->
 	</div><!-- .container -->
 </div><!-- .content -->
 
+<div class="content" id="<?php the_field('id'); ?>-content" >
+	<div class="container centered" >
+		<div class="row" >
+			<div class="col" >
+				<?php the_field('content'); ?>
+				
+				<?php if( have_rows('repeater') ): while (have_rows('repeater') ): the_row(); ?>
+				<div class="row repeater-row" >
+					<div class="col-sm-<?php the_sub_field('col-sm'); ?> col-md-<?php the_sub_field('col-lg'); ?>" >
+						<div class="image-wrapper" style="background-image:url(<?php the_sub_field('repeater_image'); ?>);" ></div>
+					</div><!-- .col -->
+					<div class="col" >
+						<?php the_sub_field('repeater_content'); ?>
+					</div><!-- .col -->
+				</div><!-- .row -->
+				<?php endwhile; endif; ?>
+				
+				<?php the_field('closing_content'); ?>
+				</div><!-- .row -->
+			</div><!-- .col -->
+		</div><!-- .row -->
+	</div><!-- .container -->
+</div><!-- .content -->
 
 <?php get_footer(); 
