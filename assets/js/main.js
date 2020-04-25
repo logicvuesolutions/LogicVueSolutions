f0448d4d5ddc791e6a425865de982114cffd67e8
Taylor Desens <taylor.desens@gmail.com>
2020-04-24 20:40:31 -0400

    Kill Fade In Animation
    Save for some real animations later

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,6 @@
 jQuery(function($){
 	
 	$(window).on("load resize", function() {
-		$('#header, #menu-toggle').fadeIn(1000);
-		$('.intro').delay(500).fadeIn(1000);
-		$('body.home .content').delay(500).fadeIn(1000);
-		$('body.body .content').delay(1000).fadeIn(1000);
-		
 		
 		$('#header, #menu-toggle, #contact-sidebar, #overlay').removeClass('open')
 		
